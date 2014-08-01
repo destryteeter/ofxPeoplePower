e9d31a8c099b66fe1c945a481f782754a4a9b7d3
Destry Teeter <destry.teeter@gmail.com>
2014-08-01 16:33:41 -0400

    Copy over sample data into example project
    simple login > get data > logout sequence

--- Example/src/ofApp.cpp
+++ Example/src/ofApp.cpp
@@ -2,60 +2,179 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    
+    ofBackground(255, 255, 255);
+    
+    //-----------
+    
+    // Choose server
+    ofxPeoplePower.prefix = ofxPeoplePower.esp_old;
+    
+    //temporarily hardcode username:password
+    ppUsername = "destry@peoplepowerco.com";
+    ppPassword = "aaaaaa";
+    
+	//the string is printed at the top of the app
+	//to give the user some feedback
+    message = "Loading mySettings.xml";
+    
+    //we load our settings file
+	//if it doesn't exist we create one
+    if (XML.loadFile("myProfile.xml")) {
+        XML.loadFile("myProfile.xml");
+        
+        // Determine if user is singed in
+        if (XML.getValue("profile:key","null") == "null") {
+            message = "Please sign in.";
+        } else {
+            // Determine if user has been signed out from another device.
+            if (XML.getAttribute("profile:key", "expires", "") == "2030-01-01T00:00:00-05:00") {
+                message = "Currently signed in";
+                
+                // Refresh device data to ensure it is up to date.
+                getDeviceData();
+                
+            } else {
+                message = "You are not longer signed in.";
+            }
+        }
+    } else {
+        setupXML();
+        message = "Unable to load myProfile.xml. New file created.";
+    }
+    
+    XML.saveFile("myProfile.xml");
+    
+    // Print XML to console
+    XML.copyXmlToString(temp);
+    cout << temp.data() << endl;
+    
+    //load a monospaced font
+	//which we will use to show part of the xml structure
+	TTF.loadFont("mono.ttf", 12);
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    
+	// Add the message bars at the top and bottom of the app
+	// ofSetHexColor(0xDDDDDD);
+	ofEnableAlphaBlending();
+	ofSetColor(0, 0, 0, 200);
+    
+	ofRect(20, 20, ofGetWidth() - 40, 74);
+    ofRect(20, ofGetHeight() - 40, ofGetWidth()-40, 24);
+    
+	// Add our key.
+	ofSetColor(240, 240, 240);
+	TTF.drawString("Sign in to account hit 'l' key.", 30, 45);
+	TTF.drawString("Sign out to account hit 'q' key.", 30, 63);
+	TTF.drawString("Get energy usage hit 'e' key.", 30, 81);
+    
+    // Add status message
+    TTF.drawString("Displaying historical energy usage: " + message, 30, ofGetHeight() - 22);
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    if(key == 'q') {
+        ofFile::removeFile("myProfile.xml");
+        XML.clear();
+        XML.clear(); // Make sure XML is blank.
+        
+        ofxPeoplePower.XML.clear();
+        ofxPeoplePower.XML.clear(); // Make sure XML is blank.
+        
+        setupXML();
+        XML.saveFile("myProfile.xml");
+        
+        message = "Signed out!";
+        
+        XML.copyXmlToString(temp); // Check XML
+        cout << "check XML: " << temp.data() << endl;
+        
+        ofxPeoplePower.XML.copyXmlToString(temp); // Check XML
+        cout << "check XML: " << temp.data() << endl;
+    }
+    if(key == 'l') {
+        
+        // get key and asign it to XML
+        ofxPeoplePower.login(ppUsername, ppPassword); // TODO: don't hardcode credentials
+        XML.pushTag("profile",0);
+        XML.setValue("key", ofxPeoplePower.XML.getValue("response:key","null"));
+        XML.setAttribute("key", "expires", ofxPeoplePower.XML.getValue("response:keyExpire","null"), 0);
+        
+        // Get user_id and location_id and asign it to XML
+        ofxPeoplePower.user(XML.getValue("key","null"));
+        XML.setValue("user_id", ofxPeoplePower.XML.getAttribute("response:user", "id", 0));
+        XML.setValue("location_id", ofxPeoplePower.XML.getAttribute("response:locations:location", "id", 0));
+        XML.popTag();
+        
+        getDeviceData();
+        
+        if (XML.getValue("profile:key","null") == "null") {
+            message = "Login failed, please try again!";
+            XML.clear();
+        } else {
+            message = "Login successful";
+            XML.saveFile();
+        }
+        
+    }
+    
+    // Load LocationEnergyUsage
+    if(key == 'e') {
+        ofxPeoplePower.locationEnergyUsage(XML.getValue("profile:key", "null"), XML.getValue("profile:location_id", "null"), "2", "2014-07-01T00:00:00", "null");
+        ofxPeoplePower.XML.copyXmlToString(temp);
+        cout << temp.data() << endl;
+        
+        message = "Success!";
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
 }
--- Example/src/ofApp.h
+++ Example/src/ofApp.h
@@ -1,22 +1,80 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxPeoplePower.h"
 
 class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+	
+    ofxPeoplePower ofxPeoplePower;
+    
+    ofxXmlSettings XML;
+    ofTrueTypeFont TTF;
+    
+    string xmlStructure;
+    string message;
+    string temp;
+    
+    string ppUsername;
+    string ppPassword;
+    
+    void setupXML() {
+        XML.popTag();
+        XML.addTag("profile");
+        XML.pushTag("profile");
+        XML.addTag("email");
+        XML.addTag("key");
+        XML.addAttribute("key", "expires", "", 0);
+        XML.addTag("user_id");
+        XML.addTag("location_id");
+        XML.addTag("devices");
+        XML.popTag();
+    }
+    
+    void getDeviceData() {
+        // Clear previous devices
+        XML.clearTagContents("profile:devices");
+        
+        // Set level to devices tag
+        XML.pushTag("profile",0);
+        XML.pushTag("devices",0);
+        
+        //Temporarily hardcode devices for my key --- TODO: Call from cloud - Currently devices must be gathered from website therefor are not up-to-date.
+        // PeoplePowerApi.devices("location_id","user_id","false");
+        ofxXmlSettings devicesXML = ofxXmlSettings("devices.xml");
+        
+        devicesXML.pushTag("response",0);
+        devicesXML.pushTag("devices",0);
+        //    cout << "getNumTags: " << devicesXML.getNumTags("device") << endl;
+        
+        // iterate through tags, copy to XML and set attributes
+        for (int i = 0; i < devicesXML.getNumTags("device"); i++ ) {
+            XML.addValue("device", devicesXML.getAttribute("device","desc","null", i));
+            // TODO: enumerate through number of attributes and setAttributes to device
+            XML.setAttribute("device", "id", devicesXML.getAttribute("device","id","null", i), i);
+            XML.setAttribute("device", "connected", devicesXML.getAttribute("device","connected","null", i), i);
+            XML.setAttribute("device", "lastDataReceivedDate", devicesXML.getAttribute("device","lastDataReceivedDate","null", i), i);
+            XML.setAttribute("device", "lastMeasureDate", devicesXML.getAttribute("device","lastMeasureDate","null", i), i);
+            XML.setAttribute("device", "type", devicesXML.getAttribute("device","type","null", i), i);
+            XML.setAttribute("device", "typeCategory", devicesXML.getAttribute("device","typeCategory","null", i), i);
+        }
+        devicesXML.popTag();
+        devicesXML.popTag();
+        XML.popTag();
+        XML.popTag();
+    }
 };
